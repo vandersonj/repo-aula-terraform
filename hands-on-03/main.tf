b711e8eb0066b38da2a9a89fdfea4e74e3e8ec40
Vanderson Jahn <vandersonj@gmail.com>
2025-07-26 14:30:25 -0300

    funções

--- hands-on-03/main.tf
+++ hands-on-03/main.tf
@@ -13,16 +13,19 @@ provider "digitalocean" {
 
 resource "digitalocean_droplet" "web" {
   image    = "ubuntu-20-04-x64"
-  name     = "vms-lab"
+  name     = "vms-lab-${count.index}"
   region   = "nyc1"
   size     = "s-1vcpu-1gb"
+  count    = 2
   ssh_keys = [digitalocean_ssh_key.ssh-key.fingerprint]
 }
 
 # Create a new SSH key
 resource "digitalocean_ssh_key" "ssh-key" {
-  name       = "Terraform Example"
+  name       = "terraform-exec"
   public_key = file("/home/vanderson/.ssh/terraform-exec.pub")
 }
-
+output "ip" {
+  value = digitalocean_droplet.web[*].ipv4_address
+}
 variable "do_token" {}
